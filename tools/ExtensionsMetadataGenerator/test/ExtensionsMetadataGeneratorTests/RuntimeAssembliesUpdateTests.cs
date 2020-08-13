7dd126cda8b720f6e2b9c9a8d722f6a355e7503f
pragnagopa <pgopa@microsoft.com>
2020-08-13 08:27:48 -0700

    Adding test.

---
+++ tools/ExtensionsMetadataGenerator/test/ExtensionsMetadataGeneratorTests/RuntimeAssembliesUpdateTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ExtensionsMetadataGenerator.Console;
+using Microsoft.Azure.WebJobs.Hosting;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace ExtensionsMetadataGeneratorTests
+{
+    public class RuntimeAssembliesUpdateTests
+    {
+        public const string ExistingRuntimeAssembliesFileName = "ExistingRuntimeAssemblies.txt";
+        public const string GeneratedRuntimeAssembliesFileName = @"../../../../../src/ExtensionsMetadataGenerator/runtimeassemblies.txt";
+        public const string DiffListFileName = "DiffList.txt";
+
+        [Fact]
+        public void VerifyGeneratedRuntimeAssemblies()
+        {
+            string[] existingRuntimeAssemblies = File.ReadAllLines(ExistingRuntimeAssembliesFileName);
+            string[] generatedRuntimeAssemblies = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), GeneratedRuntimeAssembliesFileName));
+
+            IEnumerable<string> diffAdded = generatedRuntimeAssemblies.Except(existingRuntimeAssemblies);
+            var result = diffAdded.Select(s => $"Added: {s}");
+
+            IEnumerable<string> diffRemoved = existingRuntimeAssemblies.Except(generatedRuntimeAssemblies);
+            result = result.Union(diffRemoved.Select(s => $"Removed: {s}"));
+
+            File.WriteAllLines("Result.txt", result);
+            Assert.False(result.Any(), $"Generated runtimeassemblies.txt does not match existing list. Please look at {DiffListFileName} for changes. Verify changes and update contents in {ExistingRuntimeAssembliesFileName}");
+        }
+    }
+}
